d2943f89a0cee80940ee6d3260f4e4e6c496c15d
tjaox13 <tranvinhquang48@gmail.com>
2022-11-07 23:01:26 +0700

    create project

---
+++ src/main/java/com/example/Nhom11_Microservices_SpringBoot_MySQL/Nhom11MicroservicesSpringBootMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nhom11_Microservices_SpringBoot_MySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom11MicroservicesSpringBootMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom11MicroservicesSpringBootMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Nhom11_Microservices_SpringBoot_MySQL/Nhom11MicroservicesSpringBootMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nhom11_Microservices_SpringBoot_MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom11MicroservicesSpringBootMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
